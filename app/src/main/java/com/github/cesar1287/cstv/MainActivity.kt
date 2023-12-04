737b7724cb9ecc3ed57d1ee7f75b7a7285d43620
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-12-04 20:03:44 -0300

    Adding startDestination and initial resources

--- app/src/main/java/com/github/cesar1287/cstv/MainActivity.kt
+++ app/src/main/java/com/github/cesar1287/cstv/MainActivity.kt
@@ -1,13 +1,17 @@
 package com.github.cesar1287.cstv
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.github.cesar1287.cstv.R
+import androidx.appcompat.app.AppCompatActivity
+import com.github.cesar1287.cstv.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val binding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
