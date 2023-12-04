3f05eb350628ddde56e57017eac6d2e2bd182dd6
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-12-04 19:51:04 -0300

    initial project

---
+++ app/src/main/java/com/github/cesar1287/cstv/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.github.cesar1287.cstv
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.cesar1287.cstv.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
