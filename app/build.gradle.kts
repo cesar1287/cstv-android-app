c98d8193bec88e0b90568129be5fc9f94173837b
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-12-04 21:11:31 -0300

    gradle config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,13 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
 }
+
+val apiToken: String = gradleLocalProperties(rootDir).getProperty("api_token")
 
 android {
     namespace = "com.github.cesar1287.cstv"
@@ -20,6 +26,22 @@ android {
         }
     }
 
+    flavorDimensions.add("type")
+
+    productFlavors {
+        create("dev") {
+            dimension  = "type"
+            buildConfigField("String", "BASE_URL", "\"https://api.pandascore.co/\"")
+            buildConfigField("String", "API_TOKEN", "\"$apiToken\"")
+        }
+
+        create("prod") {
+            dimension = "type"
+            buildConfigField("String", "BASE_URL", "\"https://api.pandascore.co/\"")
+            buildConfigField("String", "API_TOKEN", "\"$apiToken\"")
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -31,16 +53,17 @@ android {
     }
     
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 
     buildFeatures {
         viewBinding = true
+        buildConfig = true
     }
 
     packaging {
@@ -67,7 +90,23 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.5")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.5")
 
+    //Hilt
+    implementation("com.google.dagger:hilt-android:2.44")
+    kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    //Paging
+    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
+
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
+
+kapt {
+    correctErrorTypes = true
 }
