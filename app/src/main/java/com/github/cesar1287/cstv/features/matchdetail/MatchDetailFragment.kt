142f093dfafbdf46c263a04406ca44b01be4c08c
Cesar Rodrigues <cesar.nascimento.ufc@gmail.com>
2023-12-09 12:54:44 -0300

    adding new match detail screen

---
+++ app/src/main/java/com/github/cesar1287/cstv/features/matchdetail/MatchDetailFragment.kt
@@ -0,0 +1,23 @@
+package com.github.cesar1287.cstv.features.matchdetail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.cesar1287.cstv.databinding.FragmentMatchDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MatchDetailFragment : Fragment() {
+
+    private val binding: FragmentMatchDetailBinding by lazy {
+        FragmentMatchDetailBinding.inflate(layoutInflater)
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return binding.root
+    }
+}
